c16ddd4dfa0908a44d5bffec1584ed5692691fe1
wallisson-tech <wallissonlopes.ti@gmail.com>
2025-04-26 11:56:41 -0300

    Refatorando o seeds.rb (find_or_create_by)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,25 @@
-puts "Cadastrando moedas..."
+spinner = TTY::Spinner.new("[:spinner] Cadastrando moedas...")
+spinner.auto_spin
 
-Coin.create!(
-  description: "Bitcoin",
-  acronym: "BTC",
-  url_image: "https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=022",
-)
+  coins = [
+            { 
+              description: "Bitcoin",
+              acronym: "BTC",
+              url_image: "https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=022"
+            },
+            { 
+              description: "Ethereum",
+              acronym: "ETH",
+              url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=022"
+            },
+            { 
+              description: "Dash",
+              acronym: "DASH",
+              url_image: "https://cryptologos.cc/logos/dash-dash-logo.png?v=022"
+            },
+          ]
+  coins.each do |coin|
+    Coin.find_or_create_by!(coin)
+  end
 
-Coin.create!(
-  description: "Ethereum",
-  acronym: "ETH",
-  url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=022",
-)
-
-Coin.create!(
-  description: "Dash",
-  acronym: "DASH",
-  url_image: "https://cryptologos.cc/logos/dash-dash-logo.png?v=022",
-)
-
-puts "Moedas cadastradas com sucesso!"
+spinner.success("(Concluido!)")
