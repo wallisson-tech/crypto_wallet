3bb4ba0c192754784274dd793d0ac422a51cf289
wallisson-tech <wallissonlopes.ti@gmail.com>
2025-04-25 22:53:55 -0300

    Partials

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :coins
   root "welcome#index"
 
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   
