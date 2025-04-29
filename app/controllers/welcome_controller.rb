e86f33c52c6d346be4cb3a33f6e3c027c7532af4
wallisson-tech <wallissonlopes.ti@gmail.com>
2025-04-29 10:51:26 -0300

    Cookies e Sessions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby o Rails[COOKIE]"
+    session[:curso] = "Curso de Ruby o Rails[SESSION]"
+    @meu_nome = params[:nome]
+    @curso = params[:curso]
   end
 end
